6a1f2c68a928a83bd977ef6059208c3de0614531
haniamine <haniamiine@gmail.com>
2024-02-11 16:05:04 +0100

    add migration user

---
+++ back-spring/src/main/resources/db/migration/V110220240255__add_main_users.sql
@@ -0,0 +1,8 @@
+INSERT INTO users
+        (username, email)
+VALUES
+        ('Spring', 'haniamiine+spring@example.com'),
+        ('Batch', 'haniamiine+batch@example.com'),
+        ('Nest', 'haniamiine+nest@example.com'),
+        ('React', 'haniamiine+react@example.com'),
+        ('Angular', 'haniamiine+angular@example.com');
